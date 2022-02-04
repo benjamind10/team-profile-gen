6e8e445cbbe5fc7a4aeef1a4258906b28179fe80
Ben <benjamind10@pm.me>
2022-02-04 14:10:12 -0500

    added test for employee methods

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,3 +1,4 @@
+const { expect } = require('@jest/globals');
 const Employee = require('../lib/Employee.js');
 
 test('creates a employee object', () => {
@@ -6,4 +7,22 @@ test('creates a employee object', () => {
   expect(employee.name).toBe('Ben');
   expect(employee.id).toEqual(expect.any(Number));
   expect(employee.email).toEqual(expect.any(String));
+});
+
+test('check getName method', () => {
+  const employee = new Employee('Ben', 1, 'ben@bytebug.io');
+
+  expect(employee.getName()).toEqual(expect.any(String));
+});
+
+test('check getId method', () => {
+  const employee = new Employee('Ben', 1, 'ben@bytebug.io');
+
+  expect(employee.getId()).toEqual(expect.any(Number));
+});
+
+test('check getEmail method', () => {
+  const employee = new Employee('Ben', 1, 'benbytebug.io');
+
+  expect(employee.getEmail()).toContain('@');
 });
