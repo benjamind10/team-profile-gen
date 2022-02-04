d8779d0b5f34d6d25712ec980bfd1bebdae5b4d7
Ben <benjamind10@pm.me>
2022-02-04 13:30:45 -0500

    passed first test, and addid primary class constructor

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,8 +1,9 @@
 const Employee = require('../lib/Employee.js');
 
 test('creates a employee object', () => {
-  const employee = new Employee('Ben');
+  const employee = new Employee('Ben', 1, 'ben@bytebug.io');
 
   expect(employee.name).toBe('Ben');
   expect(employee.id).toEqual(expect.any(Number));
+  expect(employee.email).toEqual(expect.any(String));
 });
