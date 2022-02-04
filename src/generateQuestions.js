62103eb494dfd2065be232f59c2a7fbbe2d483dd
Ben <benjamind10@pm.me>
2022-02-04 17:40:11 -0500

    added manager questions

--- src/generateQuestions.js
+++ src/generateQuestions.js
@@ -15,16 +15,51 @@ const newManager = () => {
       type: 'input',
       name: 'name',
       message: 'Name of the manager:',
+      validate: validate,
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: 'Enter an ID number:',
       validate: input => {
-        if (input) return true;
-        else {
-          console.log('Please enter a name.');
+        if (isNaN(input)) {
+          console.log('Please enter a valid ID');
+          return false;
+        } else return true;
+      },
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: "Please enter the employee's email.",
+      validate: email => {
+        valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
+          email
+        );
+        if (valid) {
+          return true;
+        } else {
+          console.log('Please enter an email!');
           return false;
         }
       },
     },
+    {
+      type: 'confirm',
+      name: 'addEmployee',
+      message: 'Would you like to add more team members?',
+      default: false,
+    },
   ]);
 };
+
+function validate(arg) {
+  if (arg) return true;
+  else {
+    console.log('Please return a valid input.');
+    return false;
+  }
+}
 
 module.exports = {
   newManager,
