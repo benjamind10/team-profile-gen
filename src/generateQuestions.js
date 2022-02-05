566ad6faf64ab34e12ad73eeffb7a50a7f50e566
Ben <benjamind10@pm.me>
2022-02-04 19:59:19 -0500

    added manager to teams array

--- src/generateQuestions.js
+++ src/generateQuestions.js
@@ -10,47 +10,63 @@ const Intern = require('../lib/Intern');
 const team = [];
 
 const newManager = () => {
-  return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'Name of the manager:',
-      validate: validate,
-    },
-    {
-      type: 'input',
-      name: 'id',
-      message: 'Enter an ID number:',
-      validate: input => {
-        if (isNaN(input)) {
-          console.log('Please enter a valid ID');
-          return false;
-        } else return true;
+  return inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Name of the manager:',
+        validate: validate,
       },
-    },
-    {
-      type: 'input',
-      name: 'email',
-      message: "Please enter the employee's email.",
-      validate: email => {
-        valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
-          email
-        );
-        if (valid) {
-          return true;
-        } else {
-          console.log('Please enter an email!');
-          return false;
-        }
+      {
+        type: 'input',
+        name: 'id',
+        message: 'Enter an ID number:',
+        validate: input => {
+          if (isNaN(input)) {
+            console.log('Please enter a valid ID');
+            return false;
+          } else return true;
+        },
       },
-    },
-    {
-      type: 'confirm',
-      name: 'addEmployee',
-      message: 'Would you like to add more team members?',
-      default: false,
-    },
-  ]);
+      {
+        type: 'input',
+        name: 'email',
+        message: "Enter the employee's email.",
+        validate: email => {
+          valid =
+            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
+              email
+            );
+          if (valid) {
+            return true;
+          } else {
+            console.log('Please enter an email!');
+            return false;
+          }
+        },
+      },
+      {
+        type: 'input',
+        name: 'officeNumber',
+        message: "Enter the manager's office number",
+        validate: number_input => {
+          if (isNaN(number_input)) {
+            console.log('Please enter an office number.');
+            return false;
+          } else {
+            return true;
+          }
+        },
+      },
+    ])
+    .then(manager_data => {
+      const { name, id, email, officeNumber } = manager_data;
+      const manager = new Manager(name, id, email, officeNumber);
+
+      team.push(manager);
+      console.log(team);
+    });
 };
 
 function validate(arg) {
